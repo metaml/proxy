3ba6f8f9c0e1561d291ee53edbf6ab04ce226744
ml <metaml@gmail.com>
2016-04-21 21:24:29 -0400

    sorted imports

--- src/Network/Echo.hs
+++ src/Network/Echo.hs
@@ -2,11 +2,11 @@
 module Echo (server) where
 
 import Blaze.ByteString.Builder (fromByteString)
-import Pipes
-import qualified Pipes.Prelude as Pipes
-import Pipes.Wai
-import Network.Wai.Handler.Warp
 import Network.HTTP.Types (Status(..))
+import Network.Wai.Handler.Warp
+import Pipes
+import Pipes.Wai
+import qualified Pipes.Prelude as Pipes
 
 server = run 8000 $ \request onResponse -> do
   let p = do
